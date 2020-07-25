d76e22a90af2a141af149819e0afa2d8ba44c09b
Mike Wey <mike@mikewey.eu>
2020-07-25 17:35:21 +0200

    Enable SPAM filter.

--- source/app.d
+++ source/app.d
@@ -24,8 +24,8 @@ version(unittest) {} else
 shared static this()
 {
 	auto settings = new VibeNewsSettings;
-	//settings.spamFilters ~= new BlackListSpamFilter;
-	//settings.spamFilters ~= new BayesSpamFilter;
+	settings.spamFilters ~= new BlackListSpamFilter;
+	settings.spamFilters ~= new BayesSpamFilter;
 
 	if( existsFile("/etc/vibe/vibenews.conf") ){
 		auto data = stripUTF8Bom(cast(string)openFile("/etc/vibe/vibenews.conf").readAll());
